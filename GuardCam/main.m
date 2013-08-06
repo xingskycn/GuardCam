433ae5b2c9d2fe202abcbce74cb715226e264c65
gabrielangel <metatronangel@live.cn>
2013-08-06 09:42:29 +0800

    Initial Commit

---
+++ GuardCam/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  GuardCam
+//
+//  Created by zhou angel on 13-8-6.
+//  Copyright (c) 2013年 zhou angel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
